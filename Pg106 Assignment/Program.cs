cdeaa0067b34478feba5474a1db83595ce1d8171
Libby <ehcottle@aol.com>
2019-06-27 18:56:11 -0700

    Adding pg 106 assignment.

---
+++ Pg106 Assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        { 
+            int[] numberDivide = { 10, 47, 8, 13 };
+            Console.WriteLine("Pick a number to be divide by the array numbers.");
+            int x;
+            x = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberDivide.Length; i++)
+            {
+                int numberThree = numberDivide[i] / x;
+                Console.WriteLine(numberDivide[i] + " divided by " + x + " equals " + numberThree);
+            }
+        Console.ReadLine(); 
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please write one whole number.");
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Don't divide by zero, silly.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+    }
+}
+
