b418e5b12b0e617e53a638c7c3c3ae892a25c979
Libby <ehcottle@aol.com>
2019-06-29 11:58:11 -0700

    Adding pg 113 assignment 'void method'.

---
+++ Pg113 Assignment/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pg113_Assignment
+{
+    public class Math
+    {
+        public void Mult(int y, int z, int multi)
+        {
+            Console.WriteLine($"The equation will be you numbers ( {y} and {z} ) multiplied by ( {multi} ).");
+        }
+    }
+}
---
+++ Pg113 Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Pg113_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var methodMath = new Math();
+                Console.WriteLine("Pick a whole number for multiplication: ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick another whole number for multiplication: ");
+                int z = Convert.ToInt32(Console.ReadLine());
+                methodMath.Mult(y, z, 4);
+                Console.ReadLine();
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please write one whole number.");
+                return;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
