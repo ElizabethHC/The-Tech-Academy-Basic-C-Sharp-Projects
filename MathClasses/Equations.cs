129d8d41f31043ffde4d67165260bcd291d3a00d
Libby <ehcottle@aol.com>
2019-06-30 11:09:34 -0700

    Adding 'math classes' pg 114 assignment.

---
+++ MathClasses/Equations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    class Equation
+    {
+        public int Add(int a, int plus)
+        {
+            return a + plus;
+        }
+
+        public decimal Multiply(decimal b, int mult)
+        {
+            return b * mult;
+        }
+
+        public int stringInt(int num1, int num2, int z)
+        {
+            return num1 / num2 + z;
+        }
+    }
+}
