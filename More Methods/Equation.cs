a0926e37d04a3a99c7318c4574a6fb67a31061d0
Libby <ehcottle@aol.com>
2019-06-30 14:46:08 -0700

    Adding more methods pg 117 assignment.

---
+++ More Methods/Equation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace More_Methods
+{
+    class methodEquations
+    {
+        public int Divide(int a, int div)
+        {
+            int total = a / div;
+            return total;
+        }
+
+        public void middleDiv(int a, int b)
+        {
+            int userTotal = a + b;
+            Console.WriteLine($"The two numbers you chose ( {a} and {b} ) added together will be: {userTotal}.");
+        }
+
+        public int Divide(int firstTotal, int b, int div)
+        {
+            int total = (firstTotal + b) / div;
+            return total;
+        }
+    }
+}
