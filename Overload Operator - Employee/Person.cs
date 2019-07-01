8af077a8713f8bdb67b10567b19677dcc5e752a9
Libby <ehcottle@aol.com>
2019-07-01 15:07:21 -0700

    Adding 'overload operators' assignment.

---
+++ Overload Operator - Employee/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload_Operator___Employee
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public static Employee operator+ (Employee id, Person name)
+        {
+            id.Ids.Add(name);
+            return id;
+        }
+
+        public static Employee operator- (Employee id, Person name)
+        {
+            id.Ids.Remove(name);
+            return id;
+        }
+
+        public virtual void sayName()
+        {
+            Console.WriteLine(firstName + ", " + lastName);
+        }
+
+    }
+}
---
+++ Overload Operator - Employee/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload_Operator___Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee name = new Employee();
+            name.Ids = new List<Person>();
+            name.firstName = "Sample";
+            name.lastName = "Student";
+
+            Employee id = new Employee();
+            name += id;
+            name -= id;
+
+            Console.ReadLine();
+        }
+    }
+}
