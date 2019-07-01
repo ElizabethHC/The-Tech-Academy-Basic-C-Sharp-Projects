16fb5e4847017054922db82fa2361176a854213a
Libby <ehcottle@aol.com>
2019-07-01 12:42:08 -0700

    Adding 'polymorphism' pg 126 assignment.

---
+++ Polymorphism/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void sayName()
+        {
+            Console.WriteLine("Employee Name: ");
+            base.sayName();
+        }
+
+        //public void Id()
+        //{
+        //    Random random = new Random();
+        //    int num = random.Next();
+        //    Console.WriteLine("Employee id: " + num);
+        //}
+
+        public void Quit()
+        {
+            Console.WriteLine("An employee has resigned.");
+
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee name = new Employee();
+            name.firstName = "Sample";
+            name.lastName = "Student";
+            name.Quit();
+
+            //Employee id = new Employee();
+            //id.Id();
+            //Console.ReadLine();
+        }
+    }
+}
