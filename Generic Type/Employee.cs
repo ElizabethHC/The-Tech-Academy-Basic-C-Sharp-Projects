296edc236aa6e5c9b726118762b7fb6cec4bf412
Libby <ehcottle@aol.com>
2019-07-01 19:50:11 -0700

    Adding 'generic type' assignment.

---
+++ Generic Type/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Type
+{
+    public class Employee<T> : Person
+    {
+        public List<T> things { get; set; }
+        public string fullName { get; set; }
+        public int id { get; set; }
+    }
+}
