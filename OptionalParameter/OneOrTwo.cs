679610ff808b0b42af3657dc4ac28709eb3f8517
Libby <ehcottle@aol.com>
2019-07-08 12:07:05 -0700

    Pg 115 assignemt - Optional Parameters

--- OptionalParameter/OneOrTwo.cs
+++ OptionalParameter/OneOrTwo.cs
@@ -8,19 +8,12 @@ namespace OptionalParameter
 {
     class OneOrTwo
     {
-        public int theMath(int a, int b)
+        public int theMath(int a, int b=0)
         {
             int value1 = a + b;
             int value2 = value1 * 100;
             Console.WriteLine(a + " added to " + b + " is " + value1 + " and then multplied by 100 is " + value2); ;
             return value2;
-        }
-
-        public int theMath(int a)
-        {
-            int value = a + 100;
-            Console.WriteLine(a + " added to 100 is: " + value);
-            return value;
         }
     }
 }
