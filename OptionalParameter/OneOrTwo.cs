9623e41ad657bed5cea5945cb62f686e69cd93b4
Libby <ehcottle@aol.com>
2019-07-07 17:05:03 -0700

    Re-uploading pg 115 assignment.

---
+++ OptionalParameter/OneOrTwo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    class OneOrTwo
+    {
+        public int theMath(int a, int b)
+        {
+            int value1 = a + b;
+            int value2 = value1 * 100;
+            Console.WriteLine(a + " added to " + b + " is " + value1 + " and then multplied by 100 is " + value2); ;
+            return value2;
+        }
+
+        public int theMath(int a)
+        {
+            int value = a + 100;
+            Console.WriteLine(a + " added to 100 is: " + value);
+            return value;
+        }
+    }
+}
---
+++ OptionalParameter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a whole number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("OPTIONAL \n Pick another whole number:");
+            int b;
+            int result;
+
+            var methodMath = new OneOrTwo();
+            if (int.TryParse(Console.ReadLine(), out b))
+            {
+                result = methodMath.theMath(a, b);
+            }
+            else
+            {
+                result = methodMath.theMath(a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
