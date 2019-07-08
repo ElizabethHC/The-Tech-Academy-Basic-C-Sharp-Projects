331b7fbb3f512adbcdb55fec5e08de57f1b79538
Libby <ehcottle@aol.com>
2019-07-08 16:21:13 -0700

    Resubmit

--- DailyReportAssignment/DailyReportAssignment/Program.cs
+++ DailyReportAssignment/DailyReportAssignment/Program.cs
@@ -6,40 +6,28 @@ namespace DailyReportAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("The Tech Academy" );
+            Console.WriteLine("The Tech Academy");
             Console.WriteLine("Student Daily Report");
 
             Console.WriteLine("What course are you on?");
-            string currentCourse = Console.ReadLine();
+            var currentCourse = Console.ReadLine();
 
             Console.WriteLine("What page number?");
-            string pageNumber = Console.ReadLine();
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Do you need help with anything? Please answer “yes” or “no”.");
+            Console.WriteLine("Do you need help with anything? Please answer 'true' or 'false'.");
             var studyHelp = Console.ReadLine();
-            bool needHelp = studyHelp == "yes";
-            bool noHelp = studyHelp == "no";
-            if (noHelp)
-            {
-                Console.WriteLine("I'm glad you're doing well!");
-            }
-            else if (needHelp)
-            {
-                Console.WriteLine("Please reach out for the assistance you need!");
-            }
-            else if (!noHelp && !needHelp)
-            {
-                Console.WriteLine("I'm sorry, 'yes' or 'no' answers only.");
-            }
+            var needHelp = Convert.ToBoolean(studyHelp);
+            var noHelp = Convert.ToBoolean(studyHelp);
 
-            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
-            string posExperience = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you’d like to share? please give specifics.");
+            var posExperience = Console.ReadLine();
 
             Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
-            string giveFeedback = Console.ReadLine();
+            var giveFeedback = Console.ReadLine();
 
             Console.WriteLine("How many hours did you study today?");
-            string studyHours = Console.ReadLine();
+            int studyHours = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
             Console.ReadLine();
