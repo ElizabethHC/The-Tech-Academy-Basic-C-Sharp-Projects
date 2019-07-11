5ccf2c5423a9e51f2c258f9532807a2be1b82ef4
Libby <ehcottle@aol.com>
2019-07-11 12:41:08 -0700

    Adding polymorphism assignment.

---
+++ Polymorphism/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+
+        public void idNum()
+        {
+            Console.WriteLine("Employee id: " + Id);
+        }
+
+        public override void sayName()
+        {
+            Console.WriteLine("Employee Name: ");
+            base.sayName();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("An employee has resigned.");
+
+        }
+    }
+}
