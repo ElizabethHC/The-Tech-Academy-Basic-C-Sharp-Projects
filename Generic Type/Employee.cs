b9baee5f721c588e70d346e20d925e75fb6e7016
Libby <ehcottle@aol.com>
2019-07-14 14:45:53 -0700

    Re-submitting pg 131 'generic type' assigment.

---
+++ Generic Type/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Type
+{
+    public class Employee<T> : Person
+    {
+        internal readonly int Length;
+
+        public List<T> things { get; set; }
+        public string fullName { get; set; }
+        public int Id { get; set; }
+
+        public void idNum()
+        {
+            Console.WriteLine("Employee id: " + Id);
+        }
+    }
+}
---
+++ Generic Type/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employeePosition = new Employee<string>();
+            employeePosition.things = new List<string>() { "Customer Service Rep", "Sales Associate", "Floater", "Regional Manager", "Accountant" };
+            employeePosition.things.ForEach(i => Console.Write("{0}\n", i));
+
+            Employee<int> employeeWage = new Employee<int>();
+            employeeWage.things = new List<int>() { 10, 13, 15, 20, 23, 30 };
+            employeeWage.things.ForEach(i => Console.Write("${0}\n", i));
+
+            Console.ReadLine();
+        }
+    }
+}
