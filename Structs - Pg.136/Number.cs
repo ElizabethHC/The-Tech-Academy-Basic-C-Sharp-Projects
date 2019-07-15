d847a85c5e139048bedd3d2ba2531387ede2d96e
Libby <ehcottle@aol.com>
2019-07-14 17:58:02 -0700

    Adding pg 136 assignment 'structs'.

---
+++ Structs - Pg.136/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs___Pg._136
+{
+    struct  Number
+    {
+        public decimal Answer { get; set; }
+    }
+}
---
+++ Structs - Pg.136/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structs___Pg._136
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = 3.6;
+            decimal decNumber = System.Convert.ToDecimal(number);
+
+            Number first = new Number();
+            first.Answer = decNumber;
+            Console.WriteLine(first.Answer);
+
+            Console.ReadLine();
+        }
+    }
+}
