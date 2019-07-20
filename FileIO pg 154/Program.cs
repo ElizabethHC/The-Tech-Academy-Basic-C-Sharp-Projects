4848802fea83bf351fc0bad163ccf094b65197f6
Libby <ehcottle@aol.com>
2019-07-20 13:53:21 -0700

    Adding pg 154 assignment.

---
+++ FileIO pg 154/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO_pg_154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number.");
+            string x = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\ehcottle\logs\number.txt", x);
+            File.ReadAllText("C:\\Users\\ehcottle\\logs\\number.txt");
+
+            Console.ReadLine();
+        }
+    }
+}
