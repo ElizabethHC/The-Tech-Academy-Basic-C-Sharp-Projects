dfa99ad1b1bb7d9f21588d4807f246b53d488d60
Libby <ehcottle@aol.com>
2019-10-03 16:52:06 -0700

    SPA

---
+++ FinalChallenge/Scripts/JS.js
@@ -0,0 +1,17 @@
+﻿var theList = {};
+
+function continueOn() {
+    theList.player = $("#playerName").val();
+    theList.items = new Array();
+
+    nextPage();
+}
+
+function nextPage() {
+    $("#greet").html(theList.player);
+    $("#characterFriends").show();
+
+    $("#first").hide();
+    $("#secondPage").show();
+}
+
